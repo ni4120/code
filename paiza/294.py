5fbd3d50a78fdd451554e71f683b295d2959ef40
kokoro <lily.koko412@gmail.com>
2024-12-11 13:02:21 +0900

    ファイル整理

---
+++ paiza/294.py
@@ -0,0 +1,8 @@
+n = int(input())
+m = int(input())
+
+m2 = m * 2
+
+result = m2 - n
+
+print(result)
---
+++ paiza/739.py
@@ -0,0 +1,10 @@
+
+N = int(input())
+
+arr = []
+
+for i in range( N ):
+    num = int(input())
+    arr.append(num)
+
+new_arr = arr.sort()
