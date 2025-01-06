03d3b3dc1c58c901b2cbb28a9020a8f6b61fd2e4
kokoro <lily.koko412@gmail.com>
2025-01-06 16:40:16 +0900

    014

---
+++ math-algorithm/014.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+result = []
+
+for i in range(2,int(n ** 0.5 +1)):
+    while n % i == 0:
+        result.append(i)
+        n //= i
+if n > 1:
+    result.append(n)
+
+print(" ".join(map(str,result)))
