21f26e662af6e9bcb643c6cb46f484581b335bdf
kokoro <ni.koko4120@gmail.com>
2025-03-09 00:21:06 +0900

    396

---
+++ atcoder/396/b.py
@@ -0,0 +1,12 @@
+Q = int(input())
+stack = [0] * 100
+
+for _ in range(Q):
+    query = input().split()
+    
+    if query[0] == "1":
+        x = int(query[1])
+        stack.append(x)
+    
+    elif query[0] == "2":
+        print(stack.pop())
